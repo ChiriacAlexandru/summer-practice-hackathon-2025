176e0df7cf301b6c6ad66e5c3cf71a76d2b5ea61
DESKTOP-LILQF1B\TageStudio <Chiriac1910@gmail.com>
2025-06-06 16:56:59 +0300

    comentarii anonime, se pot incarca totusi

--- app.backend/Routes/comments.js
+++ app.backend/Routes/comments.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Comment = require('../models/Comment');
+const Comment = require('../Models/Comment');
 
 // Middleware de auth cu debugging complet
 const auth = async (req, res, next) => {
--- app.frontend/src/App.jsx
+++ app.frontend/src/App.jsx
@@ -3,21 +3,26 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Auth from './Pages/Auth';
 import NotFound from './Pages/NotFound'; 
-import Dashboard from './Pages/Dashboard';
 import Teams from './Pages/Teams';
-import Project from './Pages/Projects'
+import Project from './Pages/Projects';
 import ProjectView from './Pages/ProjectView';
+
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Auth />} />
-        <Route path="/teams" element={<Teams />} />
-        <Route path="/projects/" element={<Project />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Teams />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/projects" element={<Project />} />
+          <Route path="/projects/:projectId" element={<ProjectView />} />
+        </Route>
+
         <Route path="*" element={<NotFound />} />
-<Route path='/projects/:projectId' element={<ProjectView />} />
       </Routes>
     </Router>
   );
